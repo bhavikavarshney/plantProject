c88ee6d59879eb966eaf24a9d75cb4fd0a314cac
Dhyuti Tewani <55708942+Dhyuti@users.noreply.github.com>
2023-08-10 01:51:58 +0530

    resizing and placement of card

--- Client/src/Dashboard.jsx
+++ Client/src/Dashboard.jsx
@@ -1,40 +1,31 @@
-import React from 'react'; // Import the React library
+import React, { useState } from 'react'; // Import the React library
 
 // Define a functional component using JSX
 const Dashboard = () => {
   return (
     <>
     <div>
+      <h>Hi "name of student"</h>
+    </div>
+    <div>
       <h>My Plants</h>
     </div>
-    <div className="card w-96 bg-base-100 shadow-xl glass">
+    <div className="flex justify-center items-center h-screen">
+    <div className="card w-80 h-96 shadow-xl">
         <figure className="px-10 pt-10">
             <img alt="plant" src="/plant.jpg" className="rounded-xl" />
         </figure>
         <div className="card-body items-center text-center">
             <h2 className="card-title">Plant Name</h2>
             {/* <p>Description if required</p> */}
-        <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
-            <button className="btn btn-primary">Buy Now</button>
-            <button className="btn btn-primary">Buy Now</button>
-        </div>
-        </div>
-    </div>
-    <div className="card w-96 bg-base-100 shadow-xl glass">
-        <figure className="px-10 pt-10">
-            <img src="/plant.jpg" alt="Shoes" className="rounded-xl" />
-        </figure>
-        <div className="card-body items-center text-center">
-            <h2 className="card-title">Plant Name</h2>
-            {/* <p>Description if required</p> */}
-        <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
-            <button className="btn btn-primary">Buy Now</button>
-            <button className="btn btn-primary">Buy Now</button>
-        </div>
+            <div className="card-actions">
+                <button className="btn btn-primary">Buy Now</button>
+                <button className="btn btn-primary">Buy Now</button>
+                <button className="btn btn-primary">Buy Now</button>
+            </div>
         </div>
     </div>
+    </div>  
     </>
   );
 };
