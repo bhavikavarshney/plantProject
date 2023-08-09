14a9508a7723fe6d3a88071ab4db93993ffcff14
Dhyuti Tewani <55708942+Dhyuti@users.noreply.github.com>
2023-08-10 02:54:46 +0530

    cards

--- Client/src/Dashboard.jsx
+++ Client/src/Dashboard.jsx
@@ -11,48 +11,48 @@ const Dashboard = () => {
       <h>My Plants</h>
     </div>
     <div className="grid grid-cols-2 md:grid-cols-3 gap-6 flex justify-center items-center h-screen">
-        <div className="card w-80 h-112 shadow-xl  mx-auto">
+        <div className="card w-72 h-96 shadow-xl  mx-auto">
             <figure className="px-5 pt-5">
                 <img alt="plant" src="/plant.jpg" className="rounded-xl" />
             </figure>
             <div className="card-body items-left text-center">
-                <h2 className="card-title">Plant Name</h2>
+                <h2 className="card-title mb-6">Plant Name</h2>
                 {/* <p>Description if required</p> */}
                 <div className="card-actions">
-                    <button className="btn btn-primary">Buy Now</button>
-                    <button className="btn btn-primary">Buy Now</button>
-                    <button className="btn btn-primary">Buy Now</button>
+                    <button className="btn btn-primary w-14 h-14 mx-auto">Buy Now</button>
+                    <button className="btn btn-primary w-14 h-14 mx-auto">Buy Now</button>
+                    <button className="btn btn-primary w-14 h-14 mx-auto">Buy Now</button>
                 </div>
             </div>
         </div>
 
         
-        <div className="card w-80 h-112 shadow-xl mx-auto">
+        <div className="card w-72 h-96 shadow-xl  mx-auto">
             <figure className="px-5 pt-5">
                 <img alt="plant" src="/plant.jpg" className="rounded-xl" />
             </figure>
             <div className="card-body items-left text-center">
-                <h2 className="card-title">Plant Name</h2>
+                <h2 className="card-title mb-6">Plant Name</h2>
                 {/* <p>Description if required</p> */}
                 <div className="card-actions">
-                    <button className="btn btn-primary">Buy Now</button>
-                    <button className="btn btn-primary">Buy Now</button>
-                    <button className="btn btn-primary">Buy Now</button>
+                    <button className="btn btn-primary w-14 h-14 mx-auto">Buy Now</button>
+                    <button className="btn btn-primary w-14 h-14 mx-auto">Buy Now</button>
+                    <button className="btn btn-primary w-14 h-14 mx-auto">Buy Now</button>
                 </div>
             </div>
         </div>
 
-        <div className="card w-80 h-112 shadow-xl mx-auto">
+        <div className="card w-72 h-96 shadow-xl  mx-auto">
             <figure className="px-5 pt-5">
                 <img alt="plant" src="/plant.jpg" className="rounded-xl" />
             </figure>
             <div className="card-body items-left text-center">
-                <h2 className="card-title">Plant Name</h2>
+                <h2 className="card-title mb-6">Plant Name</h2>
                 {/* <p>Description if required</p> */}
                 <div className="card-actions">
-                    <button className="btn btn-primary">Buy Now</button>
-                    <button className="btn btn-primary">Buy Now</button>
-                    <button className="btn btn-primary">Buy Now</button>
+                    <button className="btn btn-primary w-14 h-14 mx-auto">Buy Now</button>
+                    <button className="btn btn-primary w-14 h-14 mx-auto">Buy Now</button>
+                    <button className="btn btn-primary w-14 h-14 mx-auto">Buy Now</button>
                 </div>
             </div>
         </div>
