e1a9a12df07d5ddf028fa96e5d77700b240a7a92
Dhyuti Tewani <55708942+Dhyuti@users.noreply.github.com>
2023-08-10 00:36:31 +0530

    added plant image

--- Client/src/Dashboard.jsx
+++ Client/src/Dashboard.jsx
@@ -5,12 +5,11 @@ const Dashboard = () => {
   return (
     <>
     <div>
-      <h1>Hello, JSX World!</h1>
-      <p>This is a basic JSX structure.</p>
+      <h>My Plants</h>
     </div>
     <div className="card w-96 bg-base-100 shadow-xl glass">
         <figure className="px-10 pt-10">
-            <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+            <img alt="plant" src="/plant.jpg" className="rounded-xl" />
         </figure>
         <div className="card-body items-center text-center">
             <h2 className="card-title">Shoes!</h2>
@@ -22,7 +21,7 @@ const Dashboard = () => {
     </div>
     <div className="card w-96 bg-base-100 shadow-xl glass">
         <figure className="px-10 pt-10">
-            <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+            <img src="/plant.jpg" alt="Shoes" className="rounded-xl" />
         </figure>
         <div className="card-body items-center text-center">
             <h2 className="card-title">Shoes!</h2>
